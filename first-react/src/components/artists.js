9fdfd8285e41900ae19d22beaf8d5f9397878122
surajrockzz <penugondasuraj@gmail.com>
2018-09-01 20:39:48 +0530

    added components

---
+++ first-react/src/components/artists.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class Aritists extends Component{
+    render(){
+        return(
+            <div>Artists</div>
+        )
+    }
+}
+
+export default Aritists
--- first-react/src/index.js
+++ first-react/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import {BrowserRouter,Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import Home from './components/home'
+import Artists from './components/artists'
+
+const App =()=>{
+    return(
+        <BrowserRouter>
+            <div>
+                <Route exact path ='/' component={Home}/>
+                <Route path ='/artists/:artistid' component={Artists}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+ReactDom.render(<App/>,document.getElementById('root') )
