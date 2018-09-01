3d898e158e7d7dd1d6a62842afe8e490e5b56f0c
surajrockzz <penugondasuraj@gmail.com>
2018-09-01 22:35:55 +0530

    added covers and css for covers

---
+++ first-react/src/components/artistslist.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+
+const ArtistsList = (props)=>{
+
+
+    const list = ({allArtists})=>{
+           if (allArtists){
+               return allArtists.map((item)=>{
+                   const style={
+                       background:`url(../images/covers/${item.cover}.jpg) 
+                       no-repeat`
+                   }
+                   return (
+                       <Link key={item.id} className="artist_item" to={`/artists/${item.id}`} style={style}>
+                       <div>{item.name}</div>
+                       </Link>
+                   )
+               })
+           }
+    }
+    return(
+        <div className="artists_list">
+            <h4>Browse the artists</h4>
+            {list(props)}
+        </div>
+    )
+}
+
+export default ArtistsList
