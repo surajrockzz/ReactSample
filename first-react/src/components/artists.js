40453329761672e796a35f92241306a8729e3aa4
surajrockzz <penugondasuraj@gmail.com>
2018-09-02 00:35:53 +0530

    added artist page and have to do css for artist individual page

--- first-react/src/components/artists.js
+++ first-react/src/components/artists.js
@@ -1,9 +1,46 @@
 import React,{Component} from 'react'
 
+import Header from '../components/header'
+
+const REQ_URL='http://localhost:3004/artists'
+
+
 class Aritists extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            artist:''
+        }
+    }
+
+    componentDidMount(){
+        fetch(`${REQ_URL}/${this.props.match.params.artistid}`,{
+            method:'GET'
+        })
+        .then(response => response.json())
+        .then(json=>{
+            console.log(json)
+            this.setState({
+                artist:json
+            })
+        })
+    }
     render(){
         return(
-            <div>Artists</div>
+            <div>
+            <Header/>
+                <div className="artist_bio">
+                <div className="avatar">
+                <span style={{background:`url('/images/covers/${this.state.artist.cover}.jpg') no-repeat`}}></span>
+                </div>
+            <div className="bio">
+                <h3>{this.state.artist.name}</h3>
+            </div>
+            <div className="bio_text">
+                {this.state.artist.bio}
+            </div>
+                </div>
+            </div>
         )
     }
 }
---
+++ first-react/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+const header = ()=>{
+    return (
+        <header>
+            <Link to="/">MUSIC DB</Link>
+        </header>
+    )
+}
+
+export default header;
