1436280aef86f2a190f8c919a9b38eccfd845ca9
surajrockzz <penugondasuraj@gmail.com>
2018-09-02 09:36:37 +0530

    added css for album covers

--- first-react/src/components/artists.js
+++ first-react/src/components/artists.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react'
 
 import Header from '../components/header'
-
+import AlbumList from './albumlist'
 const REQ_URL='http://localhost:3004/artists'
 
 
@@ -30,15 +30,16 @@ class Aritists extends Component{
             <div>
             <Header/>
                 <div className="artist_bio">
-                <div className="avatar">
-                <span style={{background:`url('/images/covers/${this.state.artist.cover}.jpg') no-repeat`}}></span>
-                </div>
-            <div className="bio">
-                <h3>{this.state.artist.name}</h3>
-            </div>
-            <div className="bio_text">
-                {this.state.artist.bio}
-            </div>
+                    <div className="avatar">
+                        <span style={{background:`url('/images/covers/${this.state.artist.cover}.jpg') no-repeat`}}></span>
+                    </div>
+                    <div className="bio">
+                        <h3>{this.state.artist.name}</h3>
+                    </div>
+                    <div className="bio_text">
+                        {this.state.artist.bio}
+                    </div>
+                    <AlbumList albumList={this.state.artist.albums}/>
                 </div>
             </div>
         )
